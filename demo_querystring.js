0172a4a048b0132d1c402ea72cd42493396f6847
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2020-12-07 16:41:59 +0900

    Add files via upload
    Almost at the point of solving the problem

---
+++ demo_querystring.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+
+
+var valid = fs.readFileSync("input2.txt", "utf8")
+var validArr = valid.split("\n")
+
+function validate() {
+
+    var validPasswords = 0
+
+for (var i = 0; i < validArr.length;i++) {
+    var low =  validArr[i].match(/\d+/g)[0];
+    var high = validArr[i].match(/\d+/g)[1];
+    var password = validArr[i].match(/[a-zA-Z]+/g)[1];
+    var letter = validArr[i].match(/[a-zA-Z]+/g)[0];
+
+    if (test(password, low, high, letter)) {
+        validPasswords++
+    }
+
+    console.log(letter)
+
+}
+
+
+
+}
+
+test = function(password, low, high, letter) {
+    var count = 0 
+
+    for (i=0; password.length; i++) {
+        if (password.charAt(i) === letter) {
+            count++
+        }
+
+    if (low< count && count < high) {
+        return true
+    } else {
+        return false
+    }
+
+    }
+}
+
+validate();
