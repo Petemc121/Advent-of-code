2219de6836b32f3a29ebc7023f6f8ef02188cabf
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2020-12-08 13:48:47 +0900

    final code

--- demo_querystring.js
+++ demo_querystring.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 
-
 var valid = fs.readFileSync("input2.txt", "utf8")
 var validArr = valid.split("\n")
 
@@ -8,7 +7,9 @@ function validate() {
 
     var validPasswords = 0
 
+
 for (var i = 0; i < validArr.length;i++) {
+
     var low =  validArr[i].match(/\d+/g)[0];
     var high = validArr[i].match(/\d+/g)[1];
     var password = validArr[i].match(/[a-zA-Z]+/g)[1];
@@ -18,29 +19,33 @@ for (var i = 0; i < validArr.length;i++) {
         validPasswords++
     }
 
-    console.log(letter)
-
 }
+
+
+console.log("Number of valid passwords: " + validPasswords);
 
 
 
 }
 
 test = function(password, low, high, letter) {
+
     var count = 0 
 
-    for (i=0; password.length; i++) {
+    for (i=0; i< password.length; i++) {
         if (password.charAt(i) === letter) {
             count++
         }
+       
+    }
 
-    if (low< count && count < high) {
+    if (low <= count && count <= high) {
         return true
     } else {
-        return false
+        return false 
     }
 
-    }
+    
 }
 
 validate();
